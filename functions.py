2e58516e3c7c08744f712d00d79e9cd35c939454
Daniel John <dj71286@gmail.com>
2023-07-15 01:44:37 +0100

    Implemented caching using redis

--- functions.py
+++ functions.py
@@ -1,41 +1,65 @@
 #!/usr/bin/python3
 
 import github
+import json
 from models import storage
 from github import Github
+from redis import Redis
+from datetime import timedelta
 
-g = Github(base_url="https://api.github.com", verify=False)
-check_repo_exists = "alx-system_engineering-devops"
+redis_client = Redis(host='localhost', port=6379, db=0)
 
 def get_user(username):
-    try:
-        user = g.get_user(username)
-        return user
-    except (github.GithubException, github.UnknownObjectException):
-        return None
+    user = redis_client.get(f"{username}")
+    if user is None:
+        try:
+            g = Github(base_url="https://api.github.com")
+            user_dict = vars(g.get_user(username))
+            user = {key : value for key, value in user_dict.items() if key.startswith('_rawData')}
+            user = user['_rawData']
+            redis_client.set(f"{username}", json.dumps(user))
+            redis_client.expire(f"{username}", timedelta(hours=24))
+        except (github.GithubException, github.UnknownObjectException):
+            return None
+    else:
+        user = json.loads(user)
+    return user
 
 def validate_alx(username):
-    try:
-        repo = g.get_repo(f"{username}/{check_repo_exists}")
-        if repo:
-            return True
-    except (github.GithubException, github.UnknownObjectException):
-        return None
+    repo = redis_client.get(f"{username}"+"_repo")
+    if repo is None:
+        try:
+            g = Github(base_url="https://api.github.com")
+            check_repo_exists = "alx-system_engineering-devops"
+            repo = g.get_repo(f"{username}/{check_repo_exists}")
+            if repo:
+                redis_client.set(f"{username}"+"_repo", 1)
+                redis_client.expire(f"{username}"+"_repo", timedelta(hours=24))
+        except (github.GithubException, github.UnknownObjectException):
+            return None
+    return True
 
 def get_all_repos(username):
-    try:
-        all_repos = []
-        username = g.get_user(username)
-        repos = username.get_repos()
-        for repo in repos:
-            repo_details = {
-                "name": repo.name,
-                "url": repo.html_url,
-                "description": repo.description}
-            all_repos.append(repo_details)
-        return all_repos
-    except (github.GithubException, github.UnknownObjectException):
-        return None
+    all_repos = redis_client.get(f"{username}"+"_all_repos")
+    if all_repos is None:
+        try:
+            g = Github(base_url="https://api.github.com")
+            all_repos = []
+            user = g.get_user(username)
+            repos = user.get_repos()
+            for repo in repos:
+                repo_details = {
+                    "name": repo.name,
+                    "url": repo.html_url,
+                    "description": repo.description}
+                all_repos.append(repo_details)
+            redis_client.set(f"{username}"+"_all_repos", json.dumps(all_repos))
+            redis_client.expire(f"{username}"+"_all_repos", timedelta(hours=24))
+        except (github.GithubException, github.UnknownObjectException):
+            return None
+    else:
+        all_repos = json.loads(all_repos)
+    return all_repos
 
 def get_bio(user_id):
     bio = storage.get_socials_git(user_id)
